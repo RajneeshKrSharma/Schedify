dc70f8b5f0d392d5ae796195f3b861997a97b3bc
Monu <monu.sharma@netoyed.com>
2025-07-11 16:26:35 +0530

    Schedule-list:fix

--- app/src/main/java/com/unique/schedify/post_auth/schedule_list/presentation/ScheduleListScreen.kt
+++ app/src/main/java/com/unique/schedify/post_auth/schedule_list/presentation/ScheduleListScreen.kt
@@ -55,7 +55,6 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.airbnb.lottie.compose.LottieAnimation
